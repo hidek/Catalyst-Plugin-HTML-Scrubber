fe68d3ae999472ccd88f0b74c79fe6ec60467cc7
hidek <hide@hide-k.net>
2010-07-08 13:53:25 +0900

    initial commit

---
+++ lib/Catalyst/Plugin/HTML/Scrubber.pm
@@ -0,0 +1,32 @@
+package Catalyst::Plugin::HTML::Scrubber;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Catalyst::Plugin::HTML::Scrubber -
+
+=head1 SYNOPSIS
+
+  use Catalyst::Plugin::HTML::Scrubber;
+
+=head1 DESCRIPTION
+
+Catalyst::Plugin::HTML::Scrubber is
+
+=head1 AUTHOR
+
+Hideo Kimura E<lt>hide@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
